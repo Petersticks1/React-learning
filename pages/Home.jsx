7a70d31ab43edf2612dd6a85547160c1d28cfd77
Petersticks1 <124896550+Petersticks1@users.noreply.github.com>
2024-12-18 00:50:01 +0100

    new

---
+++ pages/Home.jsx
@@ -0,0 +1,22 @@
+function Home() {
+    return (
+        <div className="home">
+            <h2>Welcome</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eaque aliquid fugit?
+                Vitae vero at, ullam voluptate, ab quasi, fuga dicta minima quam laboriosam cum. Cum
+                a magni quibusdam cupiditate. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Eum vel quisquam optio maiores similique omnis consequatur repellat sit
+                possimus, obcaecati rerum ut nulla ipsa ducimus maxime, sunt aspernatur ratione.
+                Corrupti? Est iste impedit totam rerum reiciendis ea eos itaque ducimus tenetur
+                beatae accusantium soluta, sed cumque aliquid suscipit adipisci, quisquam ratione et
+                voluptatum alias? Fuga quaerat amet quod quae earum. Nemo quos ullam fugit aperiam
+                mollitia libero ex amet distinctio pariatur alias consequuntur facilis deserunt
+                debitis dolor provident fuga, enim vitae ab sed maxime a in voluptate explicabo!
+                Porro, hic!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
